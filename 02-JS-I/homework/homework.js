f3678371fc8bab497653f65ebcf750a970bdebab
circec <circe_1975@hotmail.com>
2022-10-11 23:10:04 -0400

    update

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -212,22 +212,42 @@ redondearHaciaArriba(2.0090);
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
+  var numeroR = Math.random();
+  return numeroR;
   
 }
+numeroRandom();
+
 
 function esPositivo(numero) {
-  //La función va a recibir un entero. Devuelve como resultado una cadena de texto que indica si el número es positivo o negativo. 
+  //La función va a recibir un entero. Devuelve como resultado una cadena de texto que 
+  //indica si el número es positivo o negativo. 
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
+  if (numero > 0) {
+    return numero + "es positivo";
+    
+  }else if (numero < 0) {
+      return numero + "es negativo";
+      
+  }else if(numero = 0) {
+    return false;
+  }
   
 }
+esPositivo(1);
+
+
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+
 }
+
+
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
